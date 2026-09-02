e45674e1e7a3a205f550b12b110a11e3a5d264f3
Grisha <sokolov_gr@proton.me>
2026-09-02 23:50:55 +0200

    fix: nvim conf clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,19 @@ vim.opt.rtp:prepend(lazypath)
 
 local lazy_config = require "configs.lazy"
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
+
 -- load plugins
 require("lazy").setup({
   {
